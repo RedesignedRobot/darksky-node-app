4cffec4fea30ea1f683ffd9e7301c75af86ba241
Amir Ayub <amirayubx@gmail.com>
2019-09-18 13:48:58 +0300

    bugfixes

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const r = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/e6a1c6141f4df0dfe90bc44355d277a6/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/e6a1c6141f4df0dfe90bc44355d277a6/37.8267,-122.4233?units=si&lang=en";
 
 r({ url: url, json: true }, (e, r, b) => {
-  const data = JSON.parse(r.body);
-  console.log(data.currently.temperature);
+  console.log(b.currently.temperature);
 });
